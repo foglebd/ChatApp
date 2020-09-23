f991db56066cea80d4be57e7c09365cc427b3d27
Blake Fogle <foglebd@gmail.com>
2020-09-23 16:48:40 -0600

    server file added

---
+++ chatapp-backend/server.js
@@ -0,0 +1,17 @@
+// importing
+import express from 'express';
+
+// app config
+const app = express ();
+const port = process.env.PORT || 9000;
+// middleware
+// 
+// DB congig
+// 
+// ????
+// 
+// api routes
+app.get('/',(req,res)=>res.status(200).send('hello world'));
+// 
+// listen
+app.listen(port, () => console.log (`listening on localhost:${port}`));
